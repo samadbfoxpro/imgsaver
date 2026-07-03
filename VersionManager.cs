215275fd93cb633ff569a29442a228746dcc0060
samad baharzei <48380213+Abaharzei4@users.noreply.github.com>
2026-07-03 12:10:02 +0330

    1.8.4

--- VersionManager.cs
+++ VersionManager.cs
@@ -6,12 +6,12 @@ namespace imgsaver
 {
     public class VersionData
     {
-        public string Version { get; set; } = "1.8.3";
+        public string Version { get; set; } = "1.8.4";
     }
 
     public static class VersionManager
     {
-        public static string CurrentVersion => "1.8.3";
+        public static string CurrentVersion => "1.8.4";
 
         public static void Load() { /* No longer needed */ }
         public static void Save() { /* No longer needed */ }
